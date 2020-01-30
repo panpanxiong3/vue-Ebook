f6e5ca97c3773d72d88c2bc5be3c4ddf65d64135
panpanxiong3 <15160190801@163.com>
2020-01-30 17:30:37 +0800

    后台api接口代码

---
+++ Vue-Ebook-serve/const.js
@@ -0,0 +1,31 @@
+const resUrl = 'http://192.168.0.105:7071'
+
+const category = [
+  'Biomedicine',
+  'BusinessandManagement',
+  'ComputerScience',
+  'EarthSciences',
+  'Economics',
+  'Engineering',
+  'Education',
+  'Environment',
+  'Geography',
+  'History',
+  'Laws',
+  'LifeSciences',
+  'Literature',
+  'SocialSciences',
+  'MaterialsScience',
+  'Mathematics',
+  'MedicineAndPublicHealth',
+  'Philosophy',
+  'Physics',
+  'PoliticalScienceAndInternationalRelations',
+  'Psychology',
+  'Statistics'
+]
+
+module.exports = {
+  resUrl,
+  category,
+}
